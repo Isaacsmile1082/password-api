e422c384ac7b207d1dd55aacdd8dcb358649da02
Isaacsmile1082 <angeliss1082@gmail.com>
2022-10-04 17:16:01 -0500

    PSW-4: app finished

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const { generate } = require('generate-password');
+
+const app = express();
+const port = 8000;
+
+app.use(cors());
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
+
+
+
+app.get('/password', (req, res) => {
+    let options = req.query;
+    const parsedOptions = {
+        length: Number(options.length),
+        numbers: !(options.numbers === 'false'),
+        symbols: !(options.symbols === 'false'),
+        uppercase: !(options.uppercase === 'false'),
+        lowercase: !(options.lowercase === 'false')
+    }
+
+    console.log(parsedOptions)
+    let password = generate(parsedOptions);
+    return res.json(password)
+});
